4e341098645a424da0432e87ca59dc4db980cdd7
Biancabhl <biancahurtadolopez34@gmail.com>
2025-10-29 00:24:01 -0400

    Juego 1: MadLips. Completado.

--- Game1/mad_lips.py
+++ Game1/mad_lips.py
@@ -1,1 +1,14 @@
-#JUEGO NUMERO 1 - HISTORIAS LOCAS   
+#JUEGO NUMERO 1 - HISTORIAS LOCAS
+#Concatenar cadenas de caracteres
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo 1: ")
+verbo2 = input("Verbo 2: ")
+sustantivo_plural = input("Suatantivo plural: ")
+
+madlib = f"Programar es tan {adj}. Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
+ 
+
+
